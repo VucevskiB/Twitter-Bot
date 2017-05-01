cae515354bfdae47b5c94cecf1832817e04f77a1
Bojan Vucevski <bojanvucevski.bus@gmail.com>
2017-05-02 00:55:02 +0200

    test
    test

--- src/Main.java
+++ src/Main.java
@@ -77,7 +77,7 @@ public class Main {
 	    //System.out.println("Showing home timeline.");
 	    for (Status status : statuses) {
 	        System.out.println(status.getUser().getName() + ":" + status.getText());
-	        	if(status.isFavorited() == false && status.isRetweet() == true) {
+	        	if(status.isFavorited() == false && status.isRetweet() == false) {
 	        		twitter.createFavorite(status.getId());
 	        	}
 	        	if(status.isRetweeted() == false &&  status.isRetweet() == false ) {
